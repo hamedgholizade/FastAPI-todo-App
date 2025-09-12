1533f822cbb77dac6002496e57fcb4bd3d61fa0d
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-12 15:14:36 +0330

    in users/utils.py create generate_access_token and generate_refresh_token functions for generate and encoding data by jwt for access token and refresh token.

--- core/users/utils.py
+++ core/users/utils.py
@@ -1,5 +1,9 @@
 import secrets
+import time
+import jwt
 from passlib.context import CryptContext
+
+from core.config import settings
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -15,3 +19,31 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def generate_token(length: int = 32) -> str:
     """Generate a secure random token as a string"""
     return secrets.token_hex(length)
+
+def generate_access_token(user_id: int, expired_in: int = 60*5) -> str:
+    now = int(time.time())
+    payload = {
+        'type': 'access',
+        'user_id': user_id,
+        'iat': now,
+        'exp': now + expired_in
+    }
+    return jwt.encode(
+        payload=payload,
+        key=settings.JWT_SECRET_KEY,
+        algorithm="HS256"
+        )
+
+def generate_refresh_token(user_id: int, expired_in: int = 60*60*24) -> str:
+    now = int(time.time())
+    payload = {
+        'type': 'refresh',
+        'user_id': user_id,
+        'iat': now,
+        'exp': now + expired_in
+    }
+    return jwt.encode(
+        payload=payload,
+        key=settings.JWT_SECRET_KEY,
+        algorithm="HS256"
+        )
