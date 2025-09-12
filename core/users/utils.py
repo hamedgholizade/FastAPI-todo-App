238d468a0f75cda9be7402183c972fb56ac9a968
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-12 15:38:39 +0330

    in core/users/utils.py create function decode_refresh_token for decoding refresh token that user send .

--- core/users/utils.py
+++ core/users/utils.py
@@ -1,6 +1,11 @@
 import secrets
 import time
 import jwt
+from fastapi import HTTPException, status
+from jwt.exceptions import(
+    DecodeError,
+    InvalidSignatureError
+)
 from passlib.context import CryptContext
 
 from core.config import settings
@@ -46,4 +51,51 @@ def generate_refresh_token(user_id: int, expired_in: int = 60*60*24) -> str:
         payload=payload,
         key=settings.JWT_SECRET_KEY,
         algorithm="HS256"
+        )
+
+def decode_refresh_token(token):
+    try:
+        decoded_token = jwt.decode(
+            jwt=token,
+            key=settings.JWT_SECRET_KEY,
+            algorithms="HS256"
+            )
+        user_id = decoded_token.get("user_id", None)
+        expired_time = decoded_token.get("exp", None)
+        if user_id is None or isinstance(user_id, int)==False:
+            raise HTTPException(
+            detail="Invalid user_id in payload.",
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+        if expired_time is None or isinstance(expired_time, int)==False:
+            raise HTTPException(
+            detail="Invalid expiration time in payload.",
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+        if decoded_token.get("type", None) != "refresh":
+            raise HTTPException(
+            detail="Invalid token type.",
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+        if time.time() > expired_time:
+            raise HTTPException(
+            detail="Token expired.",
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+        return user_id
+        
+    except InvalidSignatureError:
+        raise HTTPException(
+            detail="Authentication failed, Invalid signature.",
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+    except DecodeError:
+        raise HTTPException(
+            detail="Authentication failed, Decode failed.",
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+    except Exception as e:
+        raise HTTPException(
+            detail=f"Authentication failed, {e}",
+            status_code=status.HTTP_401_UNAUTHORIZED
         )
