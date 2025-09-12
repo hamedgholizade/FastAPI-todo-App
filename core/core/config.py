c796e913fcfad46f8194a77156cc13d25ff5e9d3
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-12 15:12:20 +0330

    create virtual environment variable JWT_SECRET_KEY for secret key of encode and decode jwt athentication in .env.sample and core/config.py files.

--- core/core/config.py
+++ core/core/config.py
@@ -6,6 +6,7 @@ from pydantic_settings import (
 
 class Settings(BaseSettings):
     SQLARCHEMY_DATABASE_URL_POSTGRES: str
+    JWT_SECRET_KEY: str
     
     model_config = SettingsConfigDict(env_file=".env")
     
