087f1aa2a87a51e45507f4f08eb99dcf99f4862b
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-15 19:10:37 +0330

    in core/main.py create custom exception handler function http_exception_handler(for handling form of raising exceptions of request) and http_validation_exception_handler(for handling raising exceptions of data of request) .

--- core/main.py
+++ core/main.py
@@ -1,7 +1,10 @@
 import time
-from fastapi import FastAPI, Request
-from contextlib import asynccontextmanager
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from contextlib import asynccontextmanager
 
 from tasks.routes import router as tasks_routes
 from users.routes import router as users_routes
@@ -41,6 +44,7 @@ app = FastAPI(
 app.include_router(tasks_routes)
 app.include_router(users_routes)
 
+
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     start_time = time.perf_counter()
@@ -61,3 +65,28 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    error_response = {
+        "error": True,
+        "status_code": exc.status_code,
+        "detail": str(exc.detail),
+    }
+    return JSONResponse(status_code=exc.status_code, content=error_response)
+
+
+@app.exception_handler(RequestValidationError)
+async def http_validation_exception_handler(request, exc):
+    print(exc.__dict__)
+    error_response = {
+        "error": True,
+        "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
+        "detail": "There was a problem with your form request",
+        "content": exc.errors(),
+    }
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=error_response
+    )
