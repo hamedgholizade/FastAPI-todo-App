cf0c8aee37a351c682f37e9fd05c8c6b08868fd0
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-19 11:58:50 +0330

    create file core/core/email_util.py and write send_email function for sending email it it , and in config.py in class Settings write configuration variables for send_email ConnectionConfig .

--- core/core/config.py
+++ core/core/config.py
@@ -2,10 +2,23 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # Postgres-DB server URL
     SQLARCHEMY_DATABASE_URL_POSTGRES: str
+    # JWT Secret-key
     JWT_SECRET_KEY: str
+    # Redis server URL
     REDIS_URL: str
-
+    # Email configuration
+    MAIL_USERNAME: str = ""
+    MAIL_PASSWORD: str = ""
+    MAIL_FROM: str = "no-reply@example.com"
+    MAIL_PORT: int = 25
+    MAIL_SERVER: str = "smtp4dev"
+    MAIL_FROM_NAME: str = "Admin"
+    MAIL_STARTTLS: bool = False
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = False
+    
     model_config = SettingsConfigDict(env_file=".env")
 
 
