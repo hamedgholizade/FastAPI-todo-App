996b662991188456c61b4b61da05e20c05da5252
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-09-19 11:02:34 +0330

    installing fastapi-cache2 and add it in requirements.txt, in docker-compose.yml add redis service for running container with redis with docker, in core/core/config.py class Settings add REDIS_URL variable for url of redis server .

--- core/core/config.py
+++ core/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     SQLARCHEMY_DATABASE_URL_POSTGRES: str
     JWT_SECRET_KEY: str
+    REDIS_URL: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
